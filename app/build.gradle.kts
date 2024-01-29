4dfff26e87d5246177f8fcf662b7ff9b0649cdb7
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-29 18:38:07 +0700

    build: migrate annotation processor from kapt to ksp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
+    id("com.google.devtools.ksp")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-android")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -40,13 +44,11 @@ android {
 
 dependencies {
 
-    // CORE
+    // GENERAL
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // LIVEDATA
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 
@@ -54,8 +56,38 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 
-    // API
+    // GSON
     implementation("com.google.code.gson:gson:2.10")
+
+    // NETWORKING
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+
+    // ROOM
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+
+    // DATASTORE
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences-core:1.0.0")
+    implementation("androidx.datastore:datastore-core:1.0.0")
+
+    // GLIDE
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    // VIEWPAGER
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
+
+    // MATERIAL 3
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+
+    // RECYCLER VIEW
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
 
     // TEST
     testImplementation("junit:junit:4.13.2")
