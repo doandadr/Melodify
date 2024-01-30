a2704e4391f7bae74fd26ea7eabf8db00ff9fa56
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-31 06:36:13 +0700

    Migrate ui from livedata to flow

--- app/src/main/java/com/doanda/melodify/core/domain/usecase/TrackInteractor.kt
+++ app/src/main/java/com/doanda/melodify/core/domain/usecase/TrackInteractor.kt
@@ -1,16 +1,15 @@
 package com.doanda.melodify.core.domain.usecase
 
-import androidx.lifecycle.LiveData
 import com.doanda.melodify.core.data.Resource
-import com.doanda.melodify.core.data.TrackRepository
 import com.doanda.melodify.core.domain.model.Track
 import com.doanda.melodify.core.domain.repository.ITrackRepository
+import kotlinx.coroutines.flow.Flow
 
 class TrackInteractor(private val trackRepository: ITrackRepository ): TrackUseCase {
 
-    override fun getAllTracks(): LiveData<Resource<List<Track>>> = trackRepository.getAllTracks()
+    override fun getAllTracks(): Flow<Resource<List<Track>>> = trackRepository.getAllTracks()
 
-    override fun getFavoriteTracks(): LiveData<List<Track>> = trackRepository.getFavoriteTracks()
+    override fun getFavoriteTracks(): Flow<List<Track>> = trackRepository.getFavoriteTracks()
 
     override fun setFavoriteTrack(track: Track, state: Boolean) = trackRepository.setFavoriteTrack(track, state)
 
--- app/src/main/java/com/doanda/melodify/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/doanda/melodify/ui/favorite/FavoriteViewModel.kt
@@ -1,9 +1,9 @@
 package com.doanda.melodify.ui.favorite
 
 import androidx.lifecycle.ViewModel
-import com.doanda.melodify.core.data.TrackRepository
+import androidx.lifecycle.asLiveData
 import com.doanda.melodify.core.domain.usecase.TrackUseCase
 
 class FavoriteViewModel(trackUseCase: TrackUseCase) : ViewModel() {
-    val favoriteTracks = trackUseCase.getFavoriteTracks()
+    val favoriteTracks = trackUseCase.getFavoriteTracks().asLiveData()
 }
