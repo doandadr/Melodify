f891dd6089959a7ccb5dcae109b52b0a45e7f087
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-30 13:02:41 +0700

    add track detail layout

---
+++ app/src/main/java/com/doanda/melodify/ui/detail/TrackActivity.kt
@@ -0,0 +1,12 @@
+package com.doanda.melodify.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doanda.melodify.R
+
+class TrackActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track)
+    }
+}
