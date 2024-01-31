ea8a894319d849fa590f9580c9c20d9c3d7bea49
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-31 09:23:48 +0700

    Optimize dagger with multibindings

--- app/src/main/java/com/doanda/melodify/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/doanda/melodify/ui/favorite/FavoriteViewModel.kt
@@ -3,7 +3,8 @@ package com.doanda.melodify.ui.favorite
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.doanda.melodify.core.domain.usecase.TrackUseCase
+import javax.inject.Inject
 
-class FavoriteViewModel(trackUseCase: TrackUseCase) : ViewModel() {
+class FavoriteViewModel @Inject constructor(trackUseCase: TrackUseCase) : ViewModel() {
     val favoriteTracks = trackUseCase.getFavoriteTracks().asLiveData()
 }
--- app/src/main/java/com/doanda/melodify/ui/track/TrackViewModel.kt
+++ app/src/main/java/com/doanda/melodify/ui/track/TrackViewModel.kt
@@ -3,7 +3,8 @@ package com.doanda.melodify.ui.track
 import androidx.lifecycle.ViewModel
 import com.doanda.melodify.core.domain.model.Track
 import com.doanda.melodify.core.domain.usecase.TrackUseCase
+import javax.inject.Inject
 
-class TrackViewModel(private val trackUseCase: TrackUseCase) : ViewModel() {
+class TrackViewModel @Inject constructor(private val trackUseCase: TrackUseCase) : ViewModel() {
     fun setFavoriteTrack(track: Track, newStatus: Boolean) = trackUseCase.setFavoriteTrack(track, newStatus)
 }
