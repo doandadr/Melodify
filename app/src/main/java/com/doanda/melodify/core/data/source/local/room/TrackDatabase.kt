0abc00f51857d4dd08824f015223365b58f9cc17
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-01-31 07:11:35 +0700

    Remove unused instance declarations

--- app/src/main/java/com/doanda/melodify/core/data/source/local/room/TrackDatabase.kt
+++ app/src/main/java/com/doanda/melodify/core/data/source/local/room/TrackDatabase.kt
@@ -1,8 +1,6 @@
 package com.doanda.melodify.core.data.source.local.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.doanda.melodify.core.data.source.local.entity.TrackEntity
 
@@ -10,22 +8,4 @@ import com.doanda.melodify.core.data.source.local.entity.TrackEntity
 abstract class TrackDatabase : RoomDatabase() {
 
     abstract fun trackDao(): TrackDao
-
-    companion object {
-        @Volatile
-        private var INSTANCE: TrackDatabase? = null
-
-        fun getInstance(context: Context): TrackDatabase =
-            INSTANCE ?: synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    TrackDatabase::class.java,
-                    "track.db"
-                )
-                    .fallbackToDestructiveMigration()
-                    .build()
-                INSTANCE = instance
-                instance
-            }
-    }
 }
