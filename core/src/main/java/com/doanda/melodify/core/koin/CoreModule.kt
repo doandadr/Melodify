646ae2281c64b0552c52c42466103756f7574724
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-02-01 22:10:00 +0700

    Implement database encryption

--- core/src/main/java/com/doanda/melodify/core/koin/CoreModule.kt
+++ core/src/main/java/com/doanda/melodify/core/koin/CoreModule.kt
@@ -8,6 +8,8 @@ import com.doanda.melodify.core.data.source.remote.RemoteDataSource
 import com.doanda.melodify.core.data.source.remote.network.ApiService
 import com.doanda.melodify.core.domain.repository.ITrackRepository
 import com.doanda.melodify.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,12 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<TrackDatabase>().trackDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("melodifyapp".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             TrackDatabase::class.java, "track.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
