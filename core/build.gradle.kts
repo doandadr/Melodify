48b35fe832ad1f46cb634cdea6f0c836380e279a
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-02-01 22:01:00 +0700

    Add encryption dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -69,4 +69,8 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // ENCRYPTION
+    implementation("net.zetetic:android-database-sqlcipher:4.5.4")
+    implementation("androidx.sqlite:sqlite-ktx:2.4.0")
 }
