bff3b8641cb71d63cb45946afd68f1e605789b7f
Doanda Dresta Rahma <ddoanda@gmail.com>
2024-02-01 23:44:56 +0700

    Remove android applicatoin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,5 @@ plugins {
     kotlin("kapt") version "1.9.22"
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.android.application") version "8.2.1" apply false
     id("com.android.dynamic-feature") version "8.2.1" apply false
 }
